5c4b0574b112ec1d3c74e0195b4f65ae297daf1a
lmakoske1 <leilanni.dev@gmail.com>
2022-11-22 17:15:27 -0800

    added some form state with twoway binding for creating a parking spot, not done yet though.

--- src/parkings/pages/NewParking.js
+++ src/parkings/pages/NewParking.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewParking = () => {
-  return <div>NewParking</div>;
+  const [title, setTitle] = useState(() => "");
+  const [description, setDesc] = useState(() => "");
+
+  const onChangeHandler = (e, setValue) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  };
+
+  return (
+    <form className="parking-form">
+      <label htmlFor="parking-form-title">Title</label>
+      <input
+        id="parking-form-title"
+        type="text"
+        placeholder="gravel parking near beach..."
+        onChange={(e) => onChangeHandler(e, setTitle)}
+        value={title}
+        required
+      />
+      <label htmlFor="parking-form-description">Title</label>
+      <textarea
+        id="parking-form-description"
+        rows={5}
+        placeholder="description here..."
+        onChange={(e) => onChangeHandler(e, setDesc)}
+        value={description}
+        required
+      />
+    </form>
+  );
 };
 
 export default NewParking;
